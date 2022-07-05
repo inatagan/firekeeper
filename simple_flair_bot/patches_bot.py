4b7768403a902a92d0a8f19325fc2310a4d6d435
inatagan <22266171+inatagan@users.noreply.github.com>
2022-07-04 22:14:13 -0300

    refactor code to be pep8 conforming

--- simple_flair_bot/patches_bot.py
+++ simple_flair_bot/patches_bot.py
@@ -6,7 +6,7 @@ import re
 
 
 
-def getCommentFlair(comment):
+def get_comment_flair(comment):
     user_info = []
     try:
         user_info.append(comment.author.name)
@@ -18,7 +18,7 @@ def getCommentFlair(comment):
         return user_info[:]
 
 
-def getKarmaCount(user):
+def get_karma_count(user):
     karma = 0
     karmaCount = re.findall(r'\d+', str(user[1]).replace(' ', ''))
     for i in karmaCount:
@@ -26,7 +26,7 @@ def getKarmaCount(user):
     return karma
 
 
-def getCSSClass(karma):
+def get_css_class(karma):
     if karma >= 1000:
         user_css = subreddit_css_class[8]
     elif karma >= 500:
@@ -44,13 +44,13 @@ def getCSSClass(karma):
     return user_css
 
 
-def setFlair(sub, user, karma, user_css='green'):
+def set_flair(sub, user, karma, user_css='green'):
     newKarma = karma + 1
     new_flair_text = str(f'+{newKarma} Karma')
     sub.flair.set(user[0], new_flair_text, user_css)
 
 
-def alreadyAwarded(award_comment):
+def already_awarded(award_comment):
     count = 0
     to_user = award_comment.parent().author
     from_user = award_comment.author
@@ -111,19 +111,19 @@ def main():
                 bot_reply = comment.reply(f"Sorry /u/{comment.author} are you a cleric or something? And you are trying to award +karma to the wrong user!! \n\n ---  \n Don't forget to pop back for another visit, friend. I'll be ready to wheel and deal. Shouldst thee needeth [contact the moderators](https://www.reddit.com/message/compose?to=/r/{subreddit}&subject=About+Patches&message=) of /r/{subreddit}.")
                 bot_reply.mod.distinguish(how="yes")
                 bot_reply.mod.lock()
-            elif alreadyAwarded(comment):
+            elif already_awarded(comment):
                 bot_reply = comment.reply(f"Thought you could outwit an onion? /u/{comment.author} you have already awarded +karma to *this* user!! \n\n ---  \n Don't forget to pop back for another visit, friend. I'll be ready to wheel and deal. Shouldst thee needeth [contact the moderators](https://www.reddit.com/message/compose?to=/r/{subreddit}&subject=About+Patches&message=) of /r/{subreddit}.")
                 bot_reply.mod.distinguish(how="yes")
                 bot_reply.mod.lock() 
             else:
                 try:
-                    user = getCommentFlair(comment.parent())
+                    user = get_comment_flair(comment.parent())
                     if user[2] is not None:
-                        karma = getKarmaCount(user)
-                        user_css = getCSSClass(karma)
-                        setFlair(subreddit, user, karma, user_css)
+                        karma = get_karma_count(user)
+                        user_css = get_css_class(karma)
+                        set_flair(subreddit, user, karma, user_css)
                     else:
-                        setFlair(subreddit, user, 0)
+                        set_flair(subreddit, user, 0)
                 except:
                     bot_reply = comment.reply(f"Shame on you, you rotten cleric /u/{comment.author} something went wrong! But I'll forgive you. View it as a learning experience. At any rate, it's nice just to see you safe!  \n\n ---  \n Don't forget to pop back for another visit, friend. I'll be ready to wheel and deal. Shouldst thee needeth [contact the moderators](https://www.reddit.com/message/compose?to=/r/{subreddit}&subject=About+Patches&message=) of /r/{subreddit}.")
                     bot_reply.mod.distinguish(how="yes")
