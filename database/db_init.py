c5d6b5dd43ad448c3af47f38e18a62a3f8c2f4a3
inatagan <22266171+inatagan@users.noreply.github.com>
2022-09-23 15:25:46 -0300

    added non_participant api to database

--- database/db_init.py
+++ database/db_init.py
@@ -16,6 +16,13 @@ CREATE_KARMA_TABLE = """CREATE TABLE IF NOT EXISTS karma (
         );"""
 
 
+CREATE_NON_PARTICIPANT_TABLE = """CREATE TABLE IF NOT EXISTS non_participant(
+            id INTEGER NOT NULL PRIMARY KEY,
+            username TEXT NOT NULL UNIQUE,
+            add_date DATETIME DEFAULT CURRENT_TIMESTAMP
+        );"""
+
+
 INSERT_KARMA = """INSERT INTO karma (
             from_user,
             to_user,
@@ -25,6 +32,9 @@ INSERT_KARMA = """INSERT INTO karma (
             platform,
             subreddit)
             VALUES (?, ?, ?, ?, ?, ?, ?);"""
+
+
+INSERT_NON_PARTICIPANT = """INSERT INTO non_participant(username) VALUES (?);"""
 
 
 SYNC_KARMA_PLAT = """INSERT INTO karma (
@@ -43,25 +53,40 @@ SYNC_KARMA = """INSERT INTO karma (
             VALUES (?, ?, ?);"""
 
 
-GET_USER_KARMA = "SELECT COUNT(to_user) FROM karma WHERE to_user = ?;"
+GET_USER_KARMA = """SELECT COUNT(to_user) FROM karma WHERE to_user = ?;"""
 
 
-GET_ALL_TIME_CHAMPIONS = """SELECT to_user, COUNT(to_user) FROM karma GROUP BY to_user ORDER BY COUNT(to_user) DESC LIMIT 10;"""
+GET_ALL_TIME_CHAMPIONS = """SELECT to_user, COUNT(to_user) FROM karma WHERE to_user NOT IN (SELECT username FROM non_participant) GROUP BY to_user ORDER BY COUNT(to_user) DESC LIMIT 10;"""
 
 
-GET_WEEKLY_CHAMPIONS = "SELECT to_user, COUNT(to_user) FROM karma WHERE date BETWEEN datetime('now', '-6 days') AND datetime('now') GROUP BY to_user ORDER BY COUNT(to_user) DESC LIMIT 20;"
+GET_ALL_TIME_CHAMPIONS_BY_PLATFORM = """SELECT to_user, COUNT(to_user) FROM karma WHERE platform LIKE ? AND to_user NOT IN (SELECT username FROM non_participant) GROUP BY to_user ORDER BY COUNT(to_user) DESC LIMIT 10;"""
 
 
-GET_WEEKLY_CHAMPIONS_FROM_SUBREDDIT = "SELECT to_user, COUNT(to_user) FROM karma WHERE date BETWEEN datetime('now', '-6 days') AND datetime('now') and subreddit = ? GROUP BY to_user ORDER BY COUNT(to_user) DESC LIMIT 20;"
+GET_WEEKLY_CHAMPIONS = """SELECT to_user, COUNT(to_user) FROM karma WHERE date BETWEEN datetime('now', '-7 days') AND datetime('now') AND to_user NOT IN (SELECT username FROM non_participant) GROUP BY to_user ORDER BY COUNT(to_user) DESC LIMIT 20;"""
 
 
-GET_ALL_TIME_CHAMPIONS_FROM_SUBREDDIT = "SELECT to_user, COUNT(to_user) FROM karma WHERE subreddit = ? GROUP BY to_user ORDER BY COUNT(to_user) DESC LIMIT 10;"
+GET_WEEKLY_CHAMPIONS_BY_PLATFORM = """SELECT to_user, COUNT(to_user) FROM karma WHERE date BETWEEN datetime('now', '-7 days') AND datetime('now') AND platform LIKE ? AND to_user NOT IN (SELECT username FROM non_participant) GROUP BY to_user ORDER BY COUNT(to_user) DESC LIMIT 20;"""
 
 
-DELETE_ALL_KARMA = "DELETE FROM karma WHERE to_user = ?;"
+GET_WEEKLY_CHAMPIONS_FROM_SUBREDDIT = """SELECT to_user, COUNT(to_user) FROM karma WHERE date BETWEEN datetime('now', '-7 days') AND datetime('now') AND to_user NOT IN (SELECT username FROM non_participant) AND subreddit = ? GROUP BY to_user ORDER BY COUNT(to_user) DESC LIMIT 20;"""
 
 
-DELETE_KARMA_COMMENT_ID = "DELETE FROM karma WHERE comment_id = ?"
+GET_WEEKLY_CHAMPIONS_FROM_SUBREDDIT_BY_PLAT = """SELECT to_user, COUNT(to_user) FROM karma WHERE date BETWEEN datetime('now', '-7 days') AND datetime('now') AND to_user NOT IN (SELECT username FROM non_participant) AND subreddit = ? AND platform LIKE ? GROUP BY to_user ORDER BY COUNT(to_user) DESC LIMIT 20;"""
+
+
+GET_ALL_TIME_CHAMPIONS_FROM_SUBREDDIT = """SELECT to_user, COUNT(to_user) FROM karma WHERE to_user NOT IN (SELECT username FROM non_participant) AND subreddit = ? GROUP BY to_user ORDER BY COUNT(to_user) DESC LIMIT 10;"""
+
+
+GET_ALL_TIME_CHAMPIONS_FROM_SUBREDDIT_BY_PLAT = """SELECT to_user, COUNT(to_user) FROM karma WHERE to_user NOT IN (SELECT username FROM non_participant) AND subreddit = ? AND platform LIKE ? GROUP BY to_user ORDER BY COUNT(to_user) DESC LIMIT 10;"""
+
+
+DELETE_ALL_KARMA = """DELETE FROM karma WHERE to_user = ?;"""
+
+
+DELETE_KARMA_COMMENT_ID = """DELETE FROM karma WHERE comment_id = ?"""
+
+
+REMOVE_FROM_NON_PARTICIPANT = """DELETE FROM non_participant WHERE username = ?;"""
 
 
 # Functions
@@ -72,11 +97,22 @@ def connect():
 def create_tables(connection):
     with connection:
         connection.execute(CREATE_KARMA_TABLE)
+        connection.execute(CREATE_NON_PARTICIPANT_TABLE)
 
 
 def add_karma(connection, from_user, to_user, submission_id, comment_id, submission_title, platform, subreddit):
     with connection:
         connection.execute(INSERT_KARMA, (from_user, to_user, submission_id, comment_id, submission_title, platform, subreddit))
+
+
+def add_non_participant(connection, username):
+    with connection:
+        connection.execute(INSERT_NON_PARTICIPANT, (username,))
+
+
+def remove_from_non_participant(connection, username):
+    with connection:
+        connection.execute(REMOVE_FROM_NON_PARTICIPANT, (username,))
 
 
 def sync_karma(connection, from_user, to_user, subreddit):
@@ -99,18 +135,39 @@ def get_all_time_champions(connection):
         return connection.execute(GET_ALL_TIME_CHAMPIONS).fetchall()
 
 
+def get_all_time_champions_by_plat(connection, platform):
+    with connection:
+        return connection.execute(GET_ALL_TIME_CHAMPIONS_BY_PLATFORM, (f"{platform}%",)).fetchall()
+
+
 def get_weekly_champions(connection):
     with connection:
         return connection.execute(GET_WEEKLY_CHAMPIONS).fetchall()
+
+
+def get_weekly_champions_by_plat(connection, platform):
+    with connection:
+        return connection.execute(GET_WEEKLY_CHAMPIONS_BY_PLATFORM, (f"{platform}%",)).fetchall()
+
 
 def get_weekly_champions_from_subreddit(connection, subreddit):
     with connection:
         return connection.execute(GET_WEEKLY_CHAMPIONS_FROM_SUBREDDIT, (subreddit,)).fetchall()
 
 
+def get_weekly_champions_from_subreddit_by_plat(connection, subreddit, platform):
+    with connection:
+        return connection.execute(GET_WEEKLY_CHAMPIONS_FROM_SUBREDDIT_BY_PLAT, (subreddit, f"{platform}%")).fetchall()
+
+
 def get_all_time_champions_from_subreddit(connection, subreddit):
     with connection:
         return connection.execute(GET_ALL_TIME_CHAMPIONS_FROM_SUBREDDIT, (subreddit,)).fetchall()
+
+
+def get_all_time_champions_from_subreddit_by_plat(connection, subreddit, platform):
+    with connection:
+        return connection.execute(GET_ALL_TIME_CHAMPIONS_FROM_SUBREDDIT_BY_PLAT, (subreddit, f"{platform}%")).fetchall()
 
 
 def delete_all(connection, username):
